9cacf076cc6c9f65ab61f7f381e42bd146c664fd
Alex <zuedev@gmail.com>
2022-03-18 12:37:15 +0000

    use multiple files for tf

---
+++ gitlab.tf
@@ -0,0 +1,192 @@
+provider "gitlab" {
+  token = var.gitlab_token
+}
+
+resource "gitlab_group" "blue-bean-games" {
+  name = "Blue Bean Games"
+  path = "blue-bean-games"
+}
+
+data "gitlab_user" "grayson" {
+  username = "grayherm"
+}
+
+resource "gitlab_group_membership" "grayson" {
+  group_id     = gitlab_group.blue-bean-games.id
+  user_id      = data.gitlab_user.grayson.id
+  access_level = "developer"
+}
+
+data "gitlab_user" "toby" {
+  username = "tobuss"
+}
+
+resource "gitlab_group_membership" "toby" {
+  group_id     = gitlab_group.blue-bean-games.id
+  user_id      = data.gitlab_user.toby.id
+  access_level = "developer"
+}
+
+data "gitlab_user" "yasser" {
+  username = "yasserhcn"
+}
+
+resource "gitlab_group_membership" "yasser" {
+  group_id     = gitlab_group.blue-bean-games.id
+  user_id      = data.gitlab_user.yasser.id
+  access_level = "developer"
+}
+
+resource "gitlab_project" "terraform" {
+  name             = "Terraform"
+  path             = "terraform"
+  description      = "Our cloud infrastructure, managed by Terraform. -- https://terraform.io"
+  namespace_id     = gitlab_group.blue-bean-games.id
+  visibility_level = "public"
+
+  issues_enabled             = true
+  merge_requests_enabled     = true
+  wiki_enabled               = false
+  snippets_enabled           = false
+  container_registry_enabled = false
+  packages_enabled           = false
+  pipelines_enabled          = false
+}
+
+resource "gitlab_project_approval_rule" "terraform-1" {
+  project            = gitlab_project.terraform.id
+  name               = "terraform-1"
+  approvals_required = 2
+}
+
+resource "gitlab_project" "bluebean-dot-games" {
+  name             = "bluebean.games"
+  path             = "bluebean.games"
+  description      = "Our website! :O  -- https://bluebean.games"
+  namespace_id     = gitlab_group.blue-bean-games.id
+  visibility_level = "public"
+
+  issues_enabled             = true
+  merge_requests_enabled     = true
+  wiki_enabled               = false
+  snippets_enabled           = false
+  container_registry_enabled = false
+  packages_enabled           = false
+  pipelines_enabled          = false
+}
+
+resource "gitlab_project_approval_rule" "bluebean-dot-games-1" {
+  project            = gitlab_project.bluebean-dot-games.id
+  name               = "bluebean-dot-games-1"
+  approvals_required = 2
+}
+
+resource "gitlab_project" "Unnamed-Horror-Game" {
+  name             = "Unnamed Horror Game"
+  path             = "unnamed-horror-game"
+  description      = "Episodic collection of interactive horror stories."
+  namespace_id     = gitlab_group.blue-bean-games.id
+  visibility_level = "public"
+
+  issues_enabled             = true
+  merge_requests_enabled     = true
+  wiki_enabled               = false
+  snippets_enabled           = false
+  container_registry_enabled = false
+  packages_enabled           = false
+  pipelines_enabled          = false
+}
+
+resource "gitlab_project_approval_rule" "Unnamed-Horror-Game-1" {
+  project            = gitlab_project.Unnamed-Horror-Game.id
+  name               = "Unnamed-Horror-Game-1"
+  approvals_required = 2
+}
+
+resource "gitlab_project" "social-media" {
+  name             = "Social Media"
+  path             = "social-media"
+  description      = "A place for our long-term social media stuff."
+  namespace_id     = gitlab_group.blue-bean-games.id
+  visibility_level = "public"
+
+  issues_enabled             = true
+  merge_requests_enabled     = true
+  wiki_enabled               = false
+  snippets_enabled           = false
+  container_registry_enabled = false
+  packages_enabled           = false
+  pipelines_enabled          = false
+}
+
+resource "gitlab_project_approval_rule" "social-media-1" {
+  project            = gitlab_project.social-media.id
+  name               = "social-media-1"
+  approvals_required = 2
+}
+
+resource "gitlab_project" "Blue-Bean-Bot" {
+  name             = "Blue Bean Bot"
+  path             = "blue-bean-bot"
+  description      = "A bot for our Discord!"
+  namespace_id     = gitlab_group.blue-bean-games.id
+  visibility_level = "public"
+
+  issues_enabled             = true
+  merge_requests_enabled     = true
+  wiki_enabled               = false
+  snippets_enabled           = false
+  container_registry_enabled = true
+  packages_enabled           = false
+  pipelines_enabled          = true
+}
+
+resource "gitlab_project_approval_rule" "Blue-Bean-Bot-1" {
+  project            = gitlab_project.Blue-Bean-Bot.id
+  name               = "Blue-Bean-Bot-1"
+  approvals_required = 2
+}
+
+resource "gitlab_project" "Blue-Bean-Games" {
+  name             = "Blue Bean Games"
+  path             = "blue-bean-games"
+  description      = "A monorepo containing anything not big or specific enough to be its own project."
+  namespace_id     = gitlab_group.blue-bean-games.id
+  visibility_level = "public"
+
+  issues_enabled             = true
+  merge_requests_enabled     = true
+  wiki_enabled               = false
+  snippets_enabled           = false
+  container_registry_enabled = false
+  packages_enabled           = false
+  pipelines_enabled          = false
+}
+
+resource "gitlab_project_approval_rule" "Blue-Bean-Games-1" {
+  project            = gitlab_project.Blue-Bean-Games.id
+  name               = "Blue-Bean-Games-1"
+  approvals_required = 1
+}
+
+resource "gitlab_project" "dot-github" {
+  name             = "dot-github"
+  path             = "dot-github"
+  description      = "Root repo for our GitHub organisation -- https://github.com/Blue-Bean-Games"
+  namespace_id     = gitlab_group.blue-bean-games.id
+  visibility_level = "public"
+
+  issues_enabled             = false
+  merge_requests_enabled     = false
+  wiki_enabled               = false
+  snippets_enabled           = false
+  container_registry_enabled = false
+  packages_enabled           = false
+  pipelines_enabled          = false
+}
+
+resource "gitlab_project_approval_rule" "dot-github-1" {
+  project            = gitlab_project.dot-github.id
+  name               = "dot-github-1"
+  approvals_required = 1
+}
---
+++ google.tf
@@ -0,0 +1,117 @@
+provider "google" {
+  project = "blue-bean-games"
+  region  = "eu-west2"
+}
+
+resource "google_dns_managed_zone" "bluebean-games" {
+  name        = "bluebean-games"
+  dns_name    = "bluebean.games."
+  description = "DNS zone for domain: bluebean.games"
+
+  dnssec_config {
+    kind          = "dns#managedZoneDnsSecConfig"
+    non_existence = "nsec3"
+    state         = "on"
+
+    default_key_specs {
+      algorithm  = "rsasha256"
+      key_length = 2048
+      key_type   = "keySigning"
+      kind       = "dns#dnsKeySpec"
+    }
+
+    default_key_specs {
+      algorithm  = "rsasha256"
+      key_length = 1024
+      key_type   = "zoneSigning"
+      kind       = "dns#dnsKeySpec"
+    }
+  }
+}
+
+resource "google_dns_record_set" "bluebean-games-root-a" {
+  managed_zone = google_dns_managed_zone.bluebean-games.name
+  name         = google_dns_managed_zone.bluebean-games.dns_name
+  type         = "A"
+  ttl          = "300"
+
+  rrdatas = [
+    "185.199.108.153",
+    "185.199.109.153",
+    "185.199.110.153",
+    "185.199.111.153"
+  ]
+}
+
+resource "google_dns_record_set" "bluebean-games-root-aaaa" {
+  managed_zone = google_dns_managed_zone.bluebean-games.name
+  name         = google_dns_managed_zone.bluebean-games.dns_name
+  type         = "AAAA"
+  ttl          = "300"
+
+  rrdatas = [
+    "2606:50c0:8000::153",
+    "2606:50c0:8001::153",
+    "2606:50c0:8002::153",
+    "2606:50c0:8003::153"
+  ]
+}
+
+resource "google_dns_record_set" "bluebean-games-root-txt" {
+  managed_zone = google_dns_managed_zone.bluebean-games.name
+  name         = google_dns_managed_zone.bluebean-games.dns_name
+  type         = "TXT"
+  ttl          = "300"
+
+  rrdatas = [
+    "google-site-verification=5B0oUWwWZXNJ4q4ZlEbTfiCnTpEj12G1dVd_29t3GBw"
+  ]
+}
+
+resource "google_dns_record_set" "bluebean-games-root-mx" {
+  managed_zone = google_dns_managed_zone.bluebean-games.name
+  name         = google_dns_managed_zone.bluebean-games.dns_name
+  type         = "MX"
+  ttl          = "300"
+
+  rrdatas = [
+    "1 aspmx.l.google.com.",
+    "5 alt1.aspmx.l.google.com.",
+    "5 alt2.aspmx.l.google.com.",
+    "10 alt3.aspmx.l.google.com.",
+    "10 alt4.aspmx.l.google.com."
+  ]
+}
+
+resource "google_dns_record_set" "bluebean-games-_github-challenge-blue-bean-games-txt" {
+  managed_zone = google_dns_managed_zone.bluebean-games.name
+  name         = "_github-challenge-blue-bean-games.${google_dns_managed_zone.bluebean-games.dns_name}"
+  type         = "TXT"
+  ttl          = "300"
+
+  rrdatas = [
+    "c6a6f1b2d6"
+  ]
+}
+
+resource "google_dns_record_set" "bluebean-games-_github-pages-challenge-blue-bean-games-txt" {
+  managed_zone = google_dns_managed_zone.bluebean-games.name
+  name         = "_github-pages-challenge-blue-bean-games.${google_dns_managed_zone.bluebean-games.dns_name}"
+  type         = "TXT"
+  ttl          = "300"
+
+  rrdatas = [
+    "e2b93755c298af3c7b5831d7403675"
+  ]
+}
+
+resource "google_dns_record_set" "bluebean-games-www-cname" {
+  managed_zone = google_dns_managed_zone.bluebean-games.name
+  name         = "www.${google_dns_managed_zone.bluebean-games.dns_name}"
+  type         = "CNAME"
+  ttl          = "300"
+
+  rrdatas = [
+    "bluebean.games."
+  ]
+}
