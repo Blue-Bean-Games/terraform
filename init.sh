15086a6465a4c41d8e2e3ed96d972642e4efd22d
zuedev <zuedev@gmail.com>
2022-04-08 23:52:32 +0000

    update init.sh w/ username input

--- init.sh
+++ init.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-echo gimme dat personal access token:
+echo gitlab_username:
 
-read pat
+read gl_username
+
+echo gitlab_personal_access_token:
+
+read gl_pat
 
 terraform init \
     -backend-config="address=https://gitlab.com/api/v4/projects/34327821/terraform/state/bbg-tfstate" \
     -backend-config="lock_address=https://gitlab.com/api/v4/projects/34327821/terraform/state/bbg-tfstate/lock" \
     -backend-config="unlock_address=https://gitlab.com/api/v4/projects/34327821/terraform/state/bbg-tfstate/lock" \
-    -backend-config="username=zuedev" \
-    -backend-config="password=$pat" \
+    -backend-config="username=$gl_username" \
+    -backend-config="password=$gl_pat" \
     -backend-config="lock_method=POST" \
     -backend-config="unlock_method=DELETE" \
     -backend-config="retry_wait_min=5" $1
